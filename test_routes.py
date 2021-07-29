6020db1915501a78a28bc975936bb33324b94217
Kwame Yeboah <kwmybh@yahoo.com>
2021-07-29 02:30:06 -0400

    tests working!

--- test_routes.py
+++ test_routes.py
@@ -1,86 +1,60 @@
-from werkzeug.utils import html
-from models import User, Favorites, Recipe
-from app import app, db
+from flask import Flask, redirect, render_template, request, url_for, session  #need session
+from flask_sqlalchemy import SQLAlchemy
+
 from unittest import TestCase
+from app import app, db
+import pytest
 
-app.config['TESTING'] = True
-app.config['WTF_CSRF_ENABLED'] = False
-app.config['DEBUG'] = False
+from models import User, Favorites, Recipe
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///smart_recipe_db_test"
-app.config['TESTING'] = True
-
-db.drop_all()
-db.create_all()
-
-USER_DATA = {
-    "username":"testuser",
-    "pwd":"HASHED_PASSWORD"
-}
-
-class RouteTest(TestCase):
+class Module_Test(TestCase):
     def setUp(self):
-        User.query.delete()
-        Recipe.query.delete()
-        Favorites.query.delete()
+        app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///smart_recipe_db_test"
+        app.config['TESTING'] = True
+        db.drop_all()
+        db.create_all()     
 
-        u1 = User.register(**USER_DATA)
-        r1= Recipe(name="r1",img="bla1",vid="bla11")
-        f1= Favorites(user_id=u1.id,recipe_id=r1.id)
-        db.session.add(u1)
+        self.app = app.test_client()
 
-        self.u1 = u1
+    
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
-    def test_register(self):
-        with app.test_client() as client:
-            res = client.post('/register', data = dict(
-                                username = 'username',
-                                email = 'test@mail.com',
-                                pwd = 'password'),
-                                follow_redirects = True
-                                )
-            
-            html = res.get_data(as_text = True)
+    def login(self, name, pwd):
 
-            self.assertEqual(res.status_code, 200)
-            self.assertIn('<div class="pricing-header p-3 pb-md-4 mx-auto text-center">', html)
+        data = dict(
+            name = name,
+            pwd = pwd
+        )
+        return self.app.post('/login', data=data, follow_redirects = True)
 
-
-    def test_homepage(self):
-        with app.test_client() as client:
-            res = client.get('/')
-            html = res.get_data(as_text = True)
-
-            self.assertEqual(res.status_code, 200)
-
-            self.assertIn('<div class="pricing-header p-3 pb-md-4 mx-auto text-center">', html)
+    
+    def register(self, name, pwd):
+        data = dict(
+            name = name,
+            pwd = pwd
+        )
+        return self.app.post('/register', data=data, follow_redirects=True)
 
 
-    # def test_login(self):
-    #     with app.test_client() as client:
-    #         response = self.client.post('/login', data = dict(
-    #                             username = 'username',
-    #                             pwd = 'password'),
-    #                             follow_redirects = True
-    #                             )
-    #         self.assertIn(b'Start Here!')
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    
+    def test_login(self):
+        result = self.app.get('/login', follow_redirects = True)
+        self.assertTrue(b'<h1 class="display-4">Log In</h1>' in result.data)
+        pass
 
 
-    # def test_logout(self):
-    #     with app.test_client() as client:
-    #         self.client.post('/login', data = dict(
-    #                                 username = 'username',
-    #                                 pwd = 'password'),
-    #                                 follow_redirects = True
-    #                                 )
-    #         res = self.client.get('/logout', follow_redirects = True)
-    #         html = res.get_data(as_text = True)
+    def test_login_not_in(self):
 
-    #         self.assertEqual(res.status_code, 200)
-    #         self.assertEquals('<h3>Start Here!</h3>', html)
-        
-     
-    #  test_register
-    #  test_homepage
-    #  test_logout
-    #  test_favorites       
+        data = dict(
+            name='adasd',
+            pwd='sadgase'
+        )
+
+        result = self.app.post('/login', data=data, follow_redirects=True)
+        self.assertTrue(b'<h1 class="display-4">Log In</h1>' in result.data)
+        pass
